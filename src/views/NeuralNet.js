27dc62ea38d8d7d905365c159ffad78de3231335
NetCinematics <alpinefalcon@protonmail.com>
2023-04-18 02:02:31 -0700

    bak

--- src/views/NeuralNet.js
+++
@@ -1,4 +0,0 @@
-export default function NeuralNet(){
-return ["fill in the blank","raise all boats", "good to go", "early bird gets the worm", "get it while its hot",
-    "have your cake and eat it too"
-]}
